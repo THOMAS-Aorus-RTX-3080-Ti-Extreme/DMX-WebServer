5c9271bcc867019c9c8b36956a61a04656fddb36
THOMAS-Aorus-RTX-3080-Ti-Extreme <94693769+THOMAS-Aorus-RTX-3080-Ti-Extreme@users.noreply.github.com>
2024-05-11 12:24:07 +0200

    Update copycontent.cmd

--- copycontent.cmd
+++ copycontent.cmd
@@ -1,2 +1,15 @@
-copy cpp-header-start.txt+Html_code.html+cpp-header-stop.txt Html_code.h
+@echo off
+(
+ECHO #ifndef HTML_CODE_H
+ECHO #define HTML_CODE_H
+ECHO const char Html_code[] PROGMEM = R"rawliteral(
+) > "Html_code.h"
+
+copy Html_code.h+Html_code.html Html_code.h
+
+(
+ECHO ^)rawliteral";
+ECHO #endif
+) >> "Html_code.h"
+echo done!
 pause
